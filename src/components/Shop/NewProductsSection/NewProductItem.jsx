bf1748cb09eaab6053ab2bde10f251c59056d385
Khrystyna Yaruta <kh.yaruta@gmail.com>
2024-10-11 09:41:28 +0200

    NewProductSection is updated. Products are now fetched from the firebase.

--- src/components/Shop/NewProductsSection/NewProductItem.jsx
+++ src/components/Shop/NewProductsSection/NewProductItem.jsx
@@ -2,9 +2,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./NewProductItem.module.css";
-import productImage from "../../../assets/images/product-1.png";
 
-function NewProductItem() {
+function NewProductItem({id, name, collection, image}) {
   const [isHover, setIsHover] = useState(false);
 
   function handleStartHover() {
@@ -18,18 +17,18 @@ function NewProductItem() {
   return (
     <li className={classes["new-item"]}>
       <Link
-        to="/shop"
+        to={`/shop/${id}`}
         onMouseEnter={handleStartHover}
         onMouseLeave={handleStopHover}
       >
         <div className={classes["item-image"]}>
-          <img src={productImage} alt="product image" />
+          <img src={image} alt={name} />
           <span className={classes["new-bage"]}>New</span>
         </div>
         {isHover && (
           <div className={classes["item-info"]}>
-            <h2>Kollektion 1</h2>
-            <p>Header</p>
+            <h2>{collection}</h2>
+            <p>{name}</p>
           </div>
         )}
       </Link>
--- src/components/Shop/NewProductsSection/NewProducts.jsx
+++ src/components/Shop/NewProductsSection/NewProducts.jsx
@@ -1,17 +1,56 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchShopProducts } from "../../../util/http";
+
 import classes from "./NewProducts.module.css";
 import CtaButton from "../../UI/CTAButton";
 import NewProductItem from "./NewProductItem";
+import PageContent from "../../UI/PageContent";
+import ErrorBlock from "../../UI/ErrorBlock";
 
 function NewProductsSection() {
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: ({ signal }) => fetchShopProducts({ signal }),
+  });
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <PageContent title="Fetching...">
+        <p>Fetching items...</p>
+      </PageContent>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="Neue Produkts konnten nicht geladen werden"
+        message={
+          error.info?.message ||
+          "Neue Produkts konnten nicht abgerufen werden. Bitte versuchen Sie es später noch einmal."
+        }
+      />
+    );
+  }
+
+  if (data && !isError) {
+    const products = Object.values(data);
+    const newItems = products.slice(products.length - 4);
+    content = (
+      <ul id={classes.products}>
+        {newItems.map((item) => (
+          <NewProductItem key={item.id} {...item} />
+        ))}
+      </ul>
+    );
+  }
+  
   return (
     <section className={classes["new-products"]}>
       <h2>Herbst/Winter 2024</h2>
-      <ul id={classes.products}>
-        <NewProductItem />
-        <NewProductItem />
-        <NewProductItem />
-        <NewProductItem />
-      </ul>
+      {content}
       <CtaButton path="/shop" title="Zum Shop" />
     </section>
   );
